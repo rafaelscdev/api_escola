34b570df456bacc45fd670a243a0dfb65cc096c3
Rawan.H <rawanhawangledt@gmail.com>
2024-04-16 22:15:10 -0300

    Aula 01 Semana 12

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-const { Router } = require('express') // 
+const { Router, query } = require('express') // 
 const Aluno = require('../models/Aluno')
 const Curso = require('../models/Curso')
 
@@ -96,19 +96,34 @@ routes.post('/cursos', async (req, res) => {
 
 })
 
+
 routes.get('/cursos', async (req, res) => {
-    let params = {}
+    try {
+        let params = {}
 
-    if(req.query.nome)  {
-        params = {...params, nome: req.query.nome}
+        // SE for passado uma paramero QUERY chamado "nome" na requisição, então
+           // esse parametro "nome" é adicionado dentro da variavel params
+        if(req.query.nome)  {
+            // o ...params, cria uma cópia do params com os chaves e valores já existentes
+            params = {...params, nome: req.query.nome}
+        }
+
+        if(req.query.duracao_horas)  {
+            // o ...params, cria uma cópia do params com os chaves e valores já existentes
+            params = {...params, duracao_horas: req.query.duracao_horas}
+        }
+    
+        const cursos = await Curso.findAll({
+            where: params
+        })
+    
+        res.json(cursos)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ error: 'Não possível listar todos os cursos' })
     }
-
-    const cursos = await Curso.findAll({
-        where: params
-    })
-
-    res.json(cursos)
 })
+
 
 routes.delete('/cursos/:id', (req,res) => {
     const {id} =  req.params
@@ -124,13 +139,14 @@ routes.delete('/cursos/:id', (req,res) => {
 
 
 routes.put('/cursos/:id', async (req, res) => {
-    const id = req.params.id
+    const { id } = req.params
 
     const curso = await Curso.findByPk(id)
 
     if(!curso) {
-        return res.status(404).json({mensagem: 'Curso não encontraddo'})
+        return res.status(404).json({mensagem: 'Curso não encontrado'})
     }
+
     curso.update(req.body)
 
     await curso.save()
@@ -139,4 +155,3 @@ routes.put('/cursos/:id', async (req, res) => {
 })
 
 module.exports = routes
-
