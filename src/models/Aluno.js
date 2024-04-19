568b33bd503bc3411e43328b14431fe09cd43afe
Rawan.H <rawanhawangledt@gmail.com>
2024-04-18 22:05:24 -0300

    Rota de Login - JWT -> Aula 02 Semana 12

--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -2,6 +2,12 @@ const { DataTypes } = require('sequelize')
 const { connection } = require('../database/connection')
 
 const Aluno = connection.define('alunos', {
+    email:{
+        type: DataTypes.STRING,
+    },
+    password:{
+        type: DataTypes.STRING,
+    },
     nome: {
         type: DataTypes.STRING,
     },
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,10 +8,42 @@ routes.get('/bem_vindo', (req, res) => {
     res.json({ name: 'Bem vindo' })
 })
 
+/* ----  Rotas de Login ---- */
+
+routes.post('/login', async (req, res) => {
+    try {
+        const email = req.body.email
+        const password = req.body.password
+
+        if (!email) {
+            return res.status(400).json({ messagem: 'O email é obrigatório' })
+        }
+
+        if (!password) {
+            return res.status(400).json({ messagem: 'O password é obrigatório' })
+        }
+
+        const aluno = await Aluno.findOne({
+            where: {email:email, password:password}
+        })
+
+        if(!aluno){
+            return res.status(404).json({ messagem: 'Nenhum aluno corresponde a email e senha fornecidos!' })
+        }
+
+        res.status(200).json({"Esse é teu token JWT: JWT"})
+
+    } catch (error) {
+        return res.status(500).json({ error: error, messagem: 'Algo deu errado!' })
+    }
+})
+
 /* ----  Rotas dos Alunos ---- */
 
 routes.post('/alunos', async (req, res) => {
     try {
+        const email = req.body.email
+        const password = req.body.password
         const nome = req.body.nome
         const data_nascimento = req.body.data_nascimento
         const celular = req.body.celular
@@ -24,11 +56,13 @@ routes.post('/alunos', async (req, res) => {
             return res.status(400).json({ messagem: 'A data de nascimento é obrigatória' })
         }
 
-        if(!data_nascimento.match(/\d{4}-\d{2}-\d{2}/gm)) {
-            return res.status(400).json({ messagem: 'A data de nascimento é não está no formato correto' }) 
+        if (!data_nascimento.match(/\d{4}-\d{2}-\d{2}/gm)) {
+            return res.status(400).json({ messagem: 'A data de nascimento é não está no formato correto' })
         }
 
         const aluno = await Aluno.create({
+            email: email,
+            password: password,
             nome: nome,
             data_nascimento: data_nascimento,
             celular: celular
@@ -54,16 +88,18 @@ routes.get('/alunos/:id', async (req, res) => {
 
         const aluno = await Aluno.findByPk(id)
 
-        if(!aluno){
-            return res.status(404).json({ message: "Usuário não encontrado!"})
+        if (!aluno) {
+            return res.status(404).json({ message: "Usuário não encontrado!" })
         }
 
         res.json(aluno)
-        
+
     } catch (error) {
         console.log(error.message)
-        res.status(500).json({ error: 'Não possível listar o aluno especifico',
-                              error: error})
+        res.status(500).json({
+            error: 'Não possível listar o aluno especifico',
+            error: error
+        })
     }
 })
 
@@ -74,12 +110,13 @@ routes.post('/cursos', async (req, res) => {
         const nome = req.body.nome
         const duracao_horas = req.body.duracao_horas
 
-        if(!nome) {
-            return res.status(400).json({messagem: "O nome é obrigatório" })
+        if (!nome) {
+            return res.status(400).json({ messagem: "O nome é obrigatório" })
         }
 
-        if(!(duracao_horas >= 40 && duracao_horas <= 200)) {
-            return res.status(400).json({  messagem: "A duração do curso deve ser entre 40 e 200 horas"
+        if (!(duracao_horas >= 40 && duracao_horas <= 200)) {
+            return res.status(400).json({
+                messagem: "A duração do curso deve ser entre 40 e 200 horas"
             })
         }
 
@@ -102,21 +139,21 @@ routes.get('/cursos', async (req, res) => {
         let params = {}
 
         // SE for passado uma paramero QUERY chamado "nome" na requisição, então
-           // esse parametro "nome" é adicionado dentro da variavel params
-        if(req.query.nome)  {
+        // esse parametro "nome" é adicionado dentro da variavel params
+        if (req.query.nome) {
             // o ...params, cria uma cópia do params com os chaves e valores já existentes
-            params = {...params, nome: req.query.nome}
+            params = { ...params, nome: req.query.nome }
         }
 
-        if(req.query.duracao_horas)  {
+        if (req.query.duracao_horas) {
             // o ...params, cria uma cópia do params com os chaves e valores já existentes
-            params = {...params, duracao_horas: req.query.duracao_horas}
+            params = { ...params, duracao_horas: req.query.duracao_horas }
         }
-    
+
         const cursos = await Curso.findAll({
             where: params
         })
-    
+
         res.json(cursos)
     } catch (error) {
         console.log(error.message)
@@ -125,15 +162,15 @@ routes.get('/cursos', async (req, res) => {
 })
 
 
-routes.delete('/cursos/:id', (req,res) => {
-    const {id} =  req.params
+routes.delete('/cursos/:id', (req, res) => {
+    const { id } = req.params
 
     Curso.destroy({
         where: {
             id: id
         }
     }) // DELETE cursos from cursos where id = 1
-  
+
     res.status(204).json({})
 })
 
@@ -143,8 +180,8 @@ routes.put('/cursos/:id', async (req, res) => {
 
     const curso = await Curso.findByPk(id)
 
-    if(!curso) {
-        return res.status(404).json({mensagem: 'Curso não encontrado'})
+    if (!curso) {
+        return res.status(404).json({ mensagem: 'Curso não encontrado' })
     }
 
     curso.update(req.body)
